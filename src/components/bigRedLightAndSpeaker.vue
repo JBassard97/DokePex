0f72f9edb8fe309db74d316815a967c5ea1e4302
Jbassard <jonathanacciarito@gmail.com>
2024-03-20 02:00:57 -0400

    minor changes

--- src/components/bigRedLightAndSpeaker.vue
+++ src/components/bigRedLightAndSpeaker.vue
@@ -10,4 +10,45 @@
     </div>
 </template>
 
-<style></style>
+<style>
+.bigRedLightAndSpeaker {
+    width: 70%;
+    height: 45px;
+    position: absolute;
+    bottom: 10px;
+    display: flex;
+    justify-content: space-between;
+
+}
+
+.bigRedLight {
+    aspect-ratio: 1;
+    height: 30px;
+    border-radius: 50%;
+    background: #b91446;
+    border: 2px solid rgba(3, 5, 5, 0.5);
+}
+
+.speaker {
+    width: 30%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-between;
+}
+
+.hole {
+    width: 90%;
+    height: 5px;
+    background: #282828;
+    border-radius: 10px;
+}
+
+@media (max-width: 400px) {
+    .bigRedLightAndSpeaker {
+        bottom: 5px;
+        height: 30px;
+        margin-left: 40px;
+    }
+}
+</style>
--- src/components/screen.vue
+++ src/components/screen.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import twoRedLights from './twoRedLights.vue';
+import bigRedLightAndSpeaker from './bigRedLightAndSpeaker.vue';
 </script>
 
 <template>
@@ -8,61 +9,12 @@ import twoRedLights from './twoRedLights.vue';
             <div class="cutout"></div>
             <twoRedLights></twoRedLights>
             <div class="screen"></div>
-            <div class="bigRedLightAndSpeaker">
-                <div class="bigRedLight"></div>
-                <div class="speaker">
-                    <div class="hole"></div>
-                    <div class="hole"></div>
-                    <div class="hole"></div>
-                    <div class="hole"></div>
-                </div>
-            </div>
+            <bigRedLightAndSpeaker></bigRedLightAndSpeaker>
         </div>
     </div>
 </template>
 
 <style>
-.bigRedLightAndSpeaker {
-    width: 70%;
-    height: 45px;
-    position: absolute;
-    bottom: 10px;
-    display: flex;
-    justify-content: space-between;
-
-}
-
-@media (max-width: 400px) {
-    .bigRedLightAndSpeaker {
-        bottom: 5px;
-        height: 30px;
-        margin-left: 40px;
-    }
-}
-
-.bigRedLight {
-    aspect-ratio: 1;
-    height: 30px;
-    border-radius: 50%;
-    background: #b91446;
-    border: 2px solid rgba(3, 5, 5, 0.5);
-}
-
-.speaker {
-    width: 30%;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: space-between;
-}
-
-.hole {
-    width: 90%;
-    height: 5px;
-    background: #282828;
-    border-radius: 10px;
-}
-
 .middle {
     position: relative;
     display: flex;
